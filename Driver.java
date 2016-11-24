cccc62cad8a1390ae399fcb534c790cd6ea9b547
Robert Gevorgyan <robert1999.g@gmail.com>
2016-11-24 04:33:26 +0400

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,19 @@
+import shapes.Square;
+import shapes.Rectangle;
+import utils.Math;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Square square = new Square(100);
+		System.out.println(square.getArea());
+		
+		Rectangle rect = new Rectangle(50, 80);
+		System.out.println(rect.getArea());
+		
+		double number = 15.6;
+		System.out.println(Math.factorial((int) number));
+		System.out.println(Math.factorialLoop((int) number));
+	}
+
+}
---
+++ utils/Math.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class Math {
+	public static int factorial(int n){
+		if(n == 0)
+			return 1;
+		return n*factorial(n-1);
+	}
+	
+	public static int factorialLoop(int n) {
+		int result = 1;
+		for(int i = 1; i<=n; i++) {
+			result *= i;
+		}
+		return result;
+	}
+}
